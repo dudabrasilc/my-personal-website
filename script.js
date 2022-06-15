bcc44f7205c93a61328475354e60d064735d1453
Maria Coitinho <brasildu@gmail.com>
2022-06-15 14:53:38 -0500

    new changes to all files

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+const h1 = document.createElement("h1");
+h1.textContent = `Maria's Pre-Work`;
+document.querySelector("body").appendChild(h1);
+
